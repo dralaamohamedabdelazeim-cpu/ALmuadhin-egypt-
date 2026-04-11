ccb7dfd4d9858f8ee3875475f1e18b4ba6341ce3
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-04-11 16:26:45 +0200

    Update DismissReceiver.kt

--- app/src/main/java/com/example/almuadhin/alarm/DismissReceiver.kt
+++ app/src/main/java/com/example/almuadhin/alarm/DismissReceiver.kt
@@ -4,10 +4,13 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import androidx.core.app.NotificationManagerCompat
-
+import android.media.MediaPlayer
 class DismissReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         val notifId = intent.getIntExtra(AlarmReceiver.EXTRA_ID, 1001)
+        AzanMediaPlayer.player?.stop()
+        AzanMediaPlayer.player?.release()
+        AzanMediaPlayer.player = null
         NotificationManagerCompat.from(context).cancel(notifId)
     }
 }
